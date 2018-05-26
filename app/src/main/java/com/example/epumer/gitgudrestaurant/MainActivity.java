286c362a86d265fa30a294142dcf5a8639241fcd
eiribarren <eiribarren@iesjoandaustria.org>
2018-05-26 20:45:34 +0200

    He intentado hacer cosas

--- app/src/main/java/com/example/epumer/gitgudrestaurant/MainActivity.java
+++ app/src/main/java/com/example/epumer/gitgudrestaurant/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.epumer.gitgudrestaurant;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mIniciarComanda;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mIniciarComanda = (Button) findViewById(R.id.boto_comanda);
+        mIniciarComanda.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                escoltadorIniciarComanda(v);
+            }
+        });
+    }
+
+    public void escoltadorIniciarComanda(View v) {
+        Intent intent = new Intent(this, ComandaActivity.class);
+        startActivity(intent);
     }
 }
