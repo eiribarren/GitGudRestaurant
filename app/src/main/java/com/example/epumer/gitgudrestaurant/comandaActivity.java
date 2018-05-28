ddb0fffef4fc73e477a7ddb2659ee7ce5c06d034
eiribarren <eiribarren@iesjoandaustria.org>
2018-05-28 16:59:15 +0200

    Soy el putazo amo.

--- app/src/main/java/com/example/epumer/gitgudrestaurant/comandaActivity.java
+++ app/src/main/java/com/example/epumer/gitgudrestaurant/comandaActivity.java
@@ -18,7 +18,10 @@ public class comandaActivity extends AppCompatActivity {
         setContentView(R.layout.activity_comanda);
         mPlatsBank = new Plat[] {
                 new Plat(this, "Arroz al minecraft",3.99f,Plat.Tipus.primer, R.drawable.plato1 ),
-                new Plat(this, "Patatas rellenas",2.99f,Plat.Tipus.primer, R.drawable.patatas_rellenas )
+                new Plat(this, "Patates farcides",2.99f,Plat.Tipus.primer, R.drawable.patatas_rellenas ),
+                new Plat(this,"Albóndiga de sepia con gambón", 5.99f, Plat.Tipus.segon, R.drawable.albondiga_gambon),
+                new Plat(this,"Tarta de sorra", 1.99f, Plat.Tipus.postre, R.drawable.tarta_de_sorra),
+                new Plat(this, "Got d'aigua", 0.90f, Plat.Tipus.beguda, R.drawable.got_aigua)
         };
         for ( Plat mPlat : mPlatsBank ) {
             switch(mPlat.getTipus()) {
@@ -34,18 +37,12 @@ public class comandaActivity extends AppCompatActivity {
                 case beguda:
                     afegirPlat( mPlat, (LinearLayout) findViewById(R.id.begudes));
                     break;
-
             }
         }
     }
 
     private void afegirPlat(Plat mPlat, LinearLayout ll) {
-        mPlat.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                v.setBackgroundColor(getResources().getColor(R.color.seleccionado));
-            }
-        });
+        mPlat.setOnClickListener(mPlat);
         ll.addView(mPlat);
     }
 }
